058a81e10eaa9844d640b9a987cef3a70f0556c1
JohnZProd <zhangjoh@amazon.com>
2020-08-26 21:18:26 +1000

    First commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "fmt"
+	"net/http"
+	//"os"
+	"encoding/json"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func recommend(w http.ResponseWriter, req *http.Request) {
+	topic := req.URL.RawQuery // Everything after the ?
+
+	//Functionality to check valid topic
+
+	if topic == "" {
+		category := ""
+		topics := get_topics(category)
+
+		//Encode
+		w.Header().Set("Content-Type", "application/json") 
+		json.NewEncoder(w).Encode(topics)
+	} else {
+
+		//query_db_with_topic(topic)
+		results := query_db_with_topic(topic)
+		if len(results) == 0 {
+			// If for whatever reason no results are returned, then go to the google search for the topic
+			// This may be due to lack of communication with the db
+			fmt.Fprintf(w, "https://www.google.com/search?q="+topic)
+		} else {
+			recommendation := results[recommend_randon_number(len(results))]
+
+			//Encode
+			w.Header().Set("Content-Type", "application/json") 
+			json.NewEncoder(w).Encode(recommendation)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/api/recommend", recommend)
+	
+	http.ListenAndServe(":8090", nil)
+}
+	
---
+++ query-db.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"context"
+	"time"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"os"
+)
+
+func query_db_with_topic(topic string) []bson.M {
+	var links []bson.M
+
+	//TODO: Research Go Context
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(build_connection_string()))
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	//CHANGE THIS LINE
+	collection := client.Database("saga").Collection("links")
+	//collection := client.Database(os.Getenv("DB_NAME")).Collection("links")
+	
+	cur, err := collection.Find(ctx, bson.M{"topic": topic})
+	if err != nil { 
+		fmt.Println(err)
+		return links
+	}
+	defer cur.Close(ctx)
+
+	if err = cur.All(ctx, &links); err != nil {
+		fmt.Println(err)
+		return links
+	}
+
+	if err := cur.Err(); err != nil {
+		fmt.Println(err)
+		return links
+	}
+	return links
+}
+
+func get_topics(category string) []bson.M {
+	var topics []bson.M
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(build_connection_string()))
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	//CHANGE THIS LINE
+	collection := client.Database("saga").Collection("topics")
+	//collection := client.Database(os.Getenv("DB_NAME")).Collection("links")
+
+	cur, err := collection.Find(ctx, bson.M{})
+	if err != nil { 
+		fmt.Println(err)
+		return topics
+	}
+	defer cur.Close(ctx)
+
+	if err = cur.All(ctx, &topics); err != nil {
+		fmt.Println(err)
+		return topics
+	}
+
+	if err := cur.Err(); err != nil {
+		fmt.Println(err)
+		return topics
+	}
+	return topics
+}
+
+// TO DO
+func connect_to_db(connection_string string) {
+	//
+}
+
+func build_connection_string() string {
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_USER_PASS")
+	db_name := os.Getenv("DB_NAME")
+	endpoint := os.Getenv("DB_ENDPOINT")
+
+	return "mongodb://" +username + ":" + password + "@" + endpoint + "/" + db_name
+}
+
+func recommend_randon_number(highest int) int {
+	return rand.Intn(highest)
+}
